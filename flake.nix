3619daff446051ef735eb749bf7052ca98c51864
jonathanrlouie <jonathanrlouie@gmail.com>
2021-08-05 21:22:48 -0700

    Update flake.nix to use crate2nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,26 @@
 {
-  description = "A very basic flake";
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    crate2nix = {
+      url = "github:kolloch/crate2nix";
+      flake = false;
+    };
   };
+
+  outputs = { self, nixpkgs, flake-utils, crate2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+        crateName = "katam-randomizer";
+        inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
+          generatedCargoNix;
+        project = import (generatedCargoNix {
+          name = crateName;
+          src = ./.;
+        }) { inherit pkgs; };
+      in {
+        packages.playground = project.rootCrate.build;
+        devShell = pkgs.mkShell { buildInputs = [ project.rootCrate.build ]; };
+      }
+    );
 }
